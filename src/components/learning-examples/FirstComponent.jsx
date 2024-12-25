c2544a914d83e9845d6680d1a55e8cb8b1989359
TAEPAPER <taejeong055@gmail.com>
2024-12-25 16:36:32 +0900

    react : 컴포넌트 기본

--- src/components/learning-examples/FirstComponent.jsx
+++ src/components/learning-examples/FirstComponent.jsx
@@ -1,3 +1,31 @@
+/**
+ * react의 컴포넌트
+ * 
+ * 1.반드시 상위 태그로 감싸져야한다. / 두개의 태그를 동시에 포함할 수 없다.
+ * 
+ * 2. 컴포넌트 종류
+ *  2.1. 함수형 컴프넌트
+ *    export default function FirstComponent(){
+ *      return(
+ *        //컴포넌트 내용
+ *      )
+ *    }
+ *  2.2. class 컴포넌트
+ * 
+ *   export default class ThirdComponent extends Component{
+      render() {
+       return (
+       <div className="ThirdComponent">Third Component</div>
+        )
+      }
+    }
+ * 
+ * 3. import문에서 {중괄호} 로 감싸지 않으면 default가 import가 된다.
+ * 
+ * 
+ * 
+ * @returns 
+ */
 export default function FirstComponent() {
     return (
       <div className="FirstComponent">First Component</div>
